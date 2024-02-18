242dfff360dfb9835aaec9264e3f66c3fbf7204b
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-02-18 12:45:16 -0500

    Add files via upload

--- src/components/sections/RecentPlays/RecentPlays.tsx
+++ src/components/sections/RecentPlays/RecentPlays.tsx
@@ -106,7 +106,7 @@ export default function RecentPlays() {
           <div className="flex items-center gap-2">
             <RecentPlay event={tx} />
           </div>
-          <TimeDiff time={tx.time} suffix={md ? "ago" : ""} />
+          {TimeDiff({ time: tx.time, suffix: md ? "ago" : "" })} {/* Wrap TimeDiff in curly braces to invoke it */}
         </button>
       ))}
     </div>
