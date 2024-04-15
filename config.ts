986c40c07bfd22fb1e1838eecb9ed74a32b2aba8
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-04-15 16:59:40 -0400

    updates to latest

--- config.ts
+++ config.ts
@@ -1,8 +1,9 @@
 // config.ts
 
+import { FAKE_TOKEN_MINT, useTokenMeta } from "gamba-react-ui-v2";
+
 import { PublicKey } from "@solana/web3.js";
 import { TokenMetadata } from "./types";
-import { useTokenMeta } from "gamba-react-ui-v2";
 
 // Solana address you wish to receive fees
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
@@ -24,60 +25,46 @@ export const LIVE_EVENT_TOAST = true;
 // Platform explorer URL - Appears in welcome banner (can be changed for if you have cusotm explorer)
 export const PLATFORM_EXPLORER_URL = `https://explorer.gamba.so/platform/${PLATFORM_CREATOR_ADDRESS.toString()}`;
 
-// List of tokens supported by this platform
-export const TOKENS: Record<string, TokenMetadata> = {
+// Supported tokens
+export const TOKENLIST = [
   // SOL
-  So11111111111111111111111111111111111111112: {
+  {
     mint: new PublicKey("So11111111111111111111111111111111111111112"),
-    symbol: "SOL",
     name: "Solana",
+    symbol: "SOL",
     image:
       "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
     decimals: 9,
     baseWager: 0.01e9,
   },
   // USDC
-  EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v: {
+  {
     mint: new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
-    symbol: "USDC",
     name: "USD Coin",
+    symbol: "USDC",
     image:
-      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAMAAACdt4HsAAAAM1BMVEUAAAAgcM8oeMcndckndcondco1fs1ChtFQj9RdmNdroNt4qd6TuuW81O/X5vXl7vn///9JjkDLAAAABXRSTlMAECDg8En8Fj4AAAFcSURBVFjDvZfRQsMgDEXp2minzuX/v9YXnaXJvbktzjw1QA6BQkhagzLbQ5Z2VCyRQXMdMRuR5fzsohcmyKg9IZgso/Y5YT/mfausLyWh73d3Z3okBPMI2CEw4OYI4J8QEMe6JU51rdw+bjwl0MXCPsU+ahlhxvMnakIofh9ZRQqgB2o3ZKrsDfzP3oXCX7oq5oBvhbnAdxwQMkDmwO/nFbiwA4B5SNfmFPWjriLAHg689aM+8JUwd/dbANQxCKxBPLRnATlFAsCDJAN6xCmA2Vpfh1bv+n0UEO/5YcCNARYcPyUNXaZOI6G6MYB2G8V44HU82M2zyhGpsZ9VxUQxKhuNytiF+l24jL5M30/bggnpquLzfPZ1ThIEFshC74QzDMfmXiVJKBA6tS/TtJ+2VyVPNAw4lqrSBiVZJqqWrj+zYHhWyfMHRZeAmAYLR6l2nZD15d+K7wjBo74A10JdTYrgUpYAAAAASUVORK5CYII=",
-    decimals: 6,
-    baseWager: 1e6,
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png",
+    decimals: 9,
+    baseWager: 0.01e9,
   },
   // GUAC
-  AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR: {
+  {
     mint: new PublicKey("AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR"),
-    symbol: "GUAC",
     name: "Guacamole",
+    symbol: "GUAC",
     image:
       "https://bafkreiccbqs4jty2yjvuxp5x7gzgepquvv657ttauaqgxfhxghuz5us54u.ipfs.nftstorage.link/",
     decimals: 5,
     baseWager: 2000000e5,
   },
-  // You can easily add more tokens here
-  //
-  // "000000000000000000000000000000000000000000": {
-  //   mint: new PublicKey("000000000000000000000000000000000"),
-  //   symbol: "",
+  // ADD NEW TOKENS HERE
+  // USE TOKEN LAYOUT FOR LAYOUT STRUCTURE
+  // {
+  //   mint: new PublicKey(""),
   //   name: "",
+  //   symbol: "",
   //   image: "",
-  //   decimals: 9,
-  //   baseWager: 1e9,
+  //   decimals: 0,
+  //   baseWager: 0,
   // },
-  //
-};
-
-// Fallback handler to utilize the tokens dictionary for metadata lookup
-useTokenMeta.setFallbackHandler((mint) => {
-  const mintAddress = mint.toString();
-  const token = TOKENS[mintAddress];
-  if (token) {
-    return {
-      ...token,
-      mint: new PublicKey(token.mint),
-    };
-  }
-  return undefined;
-});
+];
