5bde332ec80f95ffead52597f4feab2658d1b214
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-05-12 20:06:37 -0400

    Huge Updates

---
+++ src/constants.ts
@@ -0,0 +1,134 @@
+// src\constants.ts
+import { PublicKey } from "@solana/web3.js";
+
+// Solana creator to receive fees
+export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
+  "GzzWXXDjLD4FDwDkWB5sARjC2aaLSfCQDjx3dmpoTY7K",
+);
+
+// Platform fees
+export const PLATFORM_CREATOR_FEE = 0.05; // 5%
+export const PLATFORM_JACKPOT_FEE = 0.01; // 1%
+
+// Toggle live toast events - all game events (true = on, false = off)
+export const LIVE_EVENT_TOAST = true;
+
+/******************************************
+ * ┌──────────────────────────────────────┐ *
+ * │          FOOTER LINKS                │ *
+ * └──────────────────────────────────────┘ *
+ ******************************************/
+
+export const FOOTER_LINKS = [
+  {
+    href: "https://github.com/BankkRoll/Gamba-V2-Next.js",
+    title: "👨‍💻 Build your own",
+  },
+  {
+    href: "https://explorer.gamba.so/create",
+    title: "🚀 Create Pool",
+  },
+  {
+    href: "https://gamba.so/docs",
+    title: "📖 Gamba Docs",
+  },
+  {
+    href: "https://discord.com/invite/HSTtFFwR",
+    title: "💬 Join Discord",
+  },
+];
+
+export const FOOTER_TWITTER_LINK = {
+  href: "https://twitter.com/bankkroll_eth",
+  title: "© 2024 Template made with ❤️ by Bankk",
+};
+
+/******************************************
+ * ┌──────────────────────────────────────┐ *
+ * │          METATAGS (SEO)              │ *
+ * └──────────────────────────────────────┘ *
+ ******************************************/
+
+export const BASE_SEO_CONFIG = {
+  defaultTitle: "Gamba - NEXTjs Demo",
+  description:
+    "The gambleFi protocol with end-to-end tools for on-chain degeneracy on Solana.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://play-gamba.vercel.app/",
+    title: "Gamba - NEXTjs Demo",
+    description:
+      "The gambleFi protocol with end-to-end tools for on-chain degeneracy on Solana.",
+    images: [
+      {
+        url: "https://play-gamba.vercel.app/seo.png",
+        alt: "Gamba - NEXTjs Demo",
+      },
+    ],
+    site_name: "Gamba - NEXTjs Demo",
+  },
+  twitter: {
+    cardType: "summary_large_image",
+    site: "https://twitter.com/gambalabs",
+    handle: "@gambalabs",
+  },
+  additionalMetaTags: [
+    {
+      name: "keywords",
+      content: "casino, gaming, rewards, gambling, entertainment",
+    },
+    {
+      name: "theme-color",
+      content: "#000000",
+    },
+  ],
+};
+
+/******************************************
+ * ┌──────────────────────────────────────┐ *
+ * │      SUPPORTED PLATFORM TOKENS       │ *
+ * └──────────────────────────────────────┘ *
+ ******************************************/
+
+export const TOKENLIST = [
+  // SOL
+  {
+    mint: new PublicKey("So11111111111111111111111111111111111111112"),
+    name: "Solana",
+    symbol: "SOL",
+    image:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
+    decimals: 9,
+    baseWager: 0.01e9,
+  },
+  // USDC
+  {
+    mint: new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
+    name: "USD Coin",
+    symbol: "USDC",
+    image:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png",
+    decimals: 9,
+    baseWager: 0.01e9,
+  },
+  // GUAC
+  {
+    mint: new PublicKey("AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR"),
+    name: "Guacamole",
+    symbol: "GUAC",
+    image:
+      "https://bafkreiccbqs4jty2yjvuxp5x7gzgepquvv657ttauaqgxfhxghuz5us54u.ipfs.nftstorage.link/",
+    decimals: 5,
+    baseWager: 2000000e5,
+  },
+  // ADD NEW TOKENS HERE
+  // {
+  //   mint: new PublicKey(""),
+  //   name: "",
+  //   symbol: "",
+  //   image: "",
+  //   decimals: 0,
+  //   baseWager: 0,
+  // },
+];
