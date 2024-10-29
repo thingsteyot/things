f41e8ff6dd80e84ab9689a3541aa3654931b139c
thingsteyot <153864999+thingsteyot@users.noreply.github.com>
2024-10-29 16:23:54 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,13 +68,6 @@ export default function HomePage() {
             </button>
           </div>
          <div className="whitespace-nowrap grid grid-cols-2 grid-rows-2 gap-2 mt-5 md:flex md:flex-col md:mt-0 md:justify-start">
-             >
-            <button
-              onClick={() => window.open("https://gamba.so/docs")}
-              className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
-            >
-              📖 Gamba Docs
-            </button>
             <button
               onClick={() => window.open("https://explorer.gamba.so")}
               className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
