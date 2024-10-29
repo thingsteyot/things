4000b61c787ca76ffcb730372356cb86e5b703db
thingsteyot <153864999+thingsteyot@users.noreply.github.com>
2024-10-29 23:29:31 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,10 +18,11 @@ export default function HomePage() {
     const referralLink = `${location.host}?code=${wallet.publicKey.toString()}`;
     navigator.clipboard.writeText(referralLink);
     toast.success(
-      `Copied! Share your link to earn a ${PLATFORM_REFERRAL_FEE * 100}% fee when players use this platform`,
+      `Copied! Share your link to earn a ${
+        PLATFORM_REFERRAL_FEE * 100
+      }% fee when players use this platform`,
     );
   };
-  
   return (
     <>
       <div className="relative mx-auto flex flex-col gap-5 mt-20 pb-10 px-2.5 transition-all duration-250 ease-in-out sm:px-5 sm:pt-5 md:max-w-6xl">
@@ -45,34 +46,34 @@ export default function HomePage() {
           <div className="absolute inset-0 bg-gradient-to-tr from-transparent via-white to-transparent opacity-10 transform rotate-12 scale-150 blur-xl pointer-events-none"></div>
 
           <div className="bg-[#15152e]/80 rounded-lg p-4 lg:col-span-2 text-center lg:text-left">
-            <div className="flex items-center justify-center md:justify-start">
+            <div className=" flex items-center justify-center md:justify-start">
               <h1 className="text-2xl md:text-4xl font-bold text-white drop-shadow-md">
-                Welcome to Things
+                Welcome to Gamba
               </h1>
               <img src="/wave.gif" className="h-14" alt="Gamba Logo" />
             </div>
             <p className="my-2 text-white drop-shadow">
-              The things gamble protocol with end-to-end tools for on-chain
+              The gambleFi protocol with end-to-end tools for on-chain
               degeneracy on Solana.
             </p>
             <p className="my-2 text-sm max-w-sm">
               Share your link to earn a {PLATFORM_REFERRAL_FEE * 100}% fee on
               each play when players use this platform using your code.
             </p>
-
             <button
-              className="bg-[#8851ff] hover:bg-[#9564ff] rounded-lg p-2 text-xs"
+              className="bg-[#8851ff] hover:bg-[#9564ff] rounded-lg p-2 text-xs bg-"
               onClick={copyInvite}
             >
-              Share Referral Link
+              Copy Link
             </button>
           </div>
           <div className="whitespace-nowrap grid grid-cols-2 grid-rows-2 gap-2 mt-5 md:flex md:flex-col md:mt-0 md:justify-start">
+         
             <button
-              onClick={() => window.open("https://explorer.gamba.so")}
+              onClick={() => window.open("https://x.com/ThingsSOL")}
               className="rounded-lg p-3 bg-white hover:bg-gray-200 hover:-translate-y-0.5 transform text-black transition-all duration-200 ease-in-out cursor-pointer shadow-lg hover:shadow-xl"
             >
-              🌐 Explorer
+              🌐 Twitter
             </button>
             <button
               onClick={() => window.open("https://discord.com/invite/HSTtFFwR")}
@@ -86,8 +87,6 @@ export default function HomePage() {
         <GameGrid />
         <h2 className="text-2xl font-bold text-center">Recent Plays</h2>
         <RecentPlays />
-          </div>
-        </div>
       </div>
     </>
   );
