e0084f10634acfd6df4f578d9c8dddc8559085b1
thingsteyot <153864999+thingsteyot@users.noreply.github.com>
2024-10-30 23:52:19 +0300

    Update index.tsx

--- src/games/index.tsx
+++ src/games/index.tsx
@@ -15,9 +15,8 @@ export const GAMES: GameBundle<GameMeta>[] = [
   {
     id: "flip",
     meta: {
-      background: "#e8dccb",
-      name: "Flip",
       background: "url('https://i.hizliresim.com/rpiy3nq.png')",
+      name: "Flip",      
       description: `Flip presents a compelling blend of simplicity and high-stakes excitement, where players face the tantalizing gamble of choosing between Heads or Tails. Every flip of the coin serves as a decisive moment, offering the potential to double one's fortune or lose it all. This straightforward yet adrenaline-inducing game tests players' luck and strategic decision-making prowess, ensuring an electrifying and immersive gaming session.`,
       volatility: 1,
     },
