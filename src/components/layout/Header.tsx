21632a0b4deb85070c167d43e4bd3e9bbf55144b
thingsteyot <153864999+thingsteyot@users.noreply.github.com>
2024-10-31 11:24:17 +0300

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -10,7 +10,7 @@ import {
 import { useState } from "react";
 
 import Link from "next/link";
-import { Modal } from "@/components/ui/Modal";
+
 import TokenSelect from "../ui/TokenSelect";
 import { UserButton } from "../ui/UserButton";
 
