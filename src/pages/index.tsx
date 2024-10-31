c558f034bb173f78070de7401442484b1cb5b8c2
thingsteyot <153864999+thingsteyot@users.noreply.github.com>
2024-10-31 12:43:15 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,3 +92,14 @@ export default function HomePage() {
     </>
   );
 }
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-EMKTDDKKV0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-EMKTDDKKV0');
+</script>
+</head>
