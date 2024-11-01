c796bbb93597b83e644a76b114cae4ac1cbce2ce
thingsteyot <153864999+thingsteyot@users.noreply.github.com>
2024-11-01 23:17:02 +0300

    Update GameGrid.tsx

--- src/components/game/GameGrid.tsx
+++ src/components/game/GameGrid.tsx
@@ -4,9 +4,9 @@ import { GAMES } from "@/games";
 import { GameCard } from "./GameCard";
 import React from "react";
 
-export function GameGrid() {
+export function GameGrid() { // Dışa aktarma burada yapılmalı
   return (
-    <div className="flex flex-col overflow-y-scroll h-[600px] space-y-4 p-4">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
       {GAMES.map((game) => (
         <GameCard key={game.id} game={game} />
       ))}
